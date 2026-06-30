99456424317f66c20523851c5698df662e59b59e
Dalienst <oduordalienst@gmail.com>
2026-06-30 11:07:48 +0300

    update serializer

--- journalbatches/serializers.py
+++ journalbatches/serializers.py
@@ -18,6 +18,7 @@ class JournalBatchSerializer(serializers.ModelSerializer):
             "posted",
             "created_at",
             "updated_at",
+            "posting_date",
             "reference",
             "entries",
         )
